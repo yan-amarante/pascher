4417bbea122dceb48fd75ff605e8d760a3cadccb
Yan Amarante <yanlimaamarantec@hotmail.com>
2024-10-25 02:55:46 -0300

    feat: add springboot basic template

---
+++ back-end/src/test/java/com/pascher/back_end/PascherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascher.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PascherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
